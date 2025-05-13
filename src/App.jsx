cd1d8912d4bb097b2a86f4252d702b6661538d05
demo <suryamgowda93@gmail.com>
2025-05-13 13:29:27 +0530

    working on dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -39,14 +39,7 @@ export default function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/authorized" element={<Authorized />} />
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/dashboard"element={<ProtectedRoute><Dashboard /> </ProtectedRoute>}      />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
